46183e767ba8404a952eccbbeeb9715968bd8e78
viola <weifang.zeng@happylifeplat.com>
2017-06-30 17:40:57 +0800

    删除dist

---
+++ src/views/list/list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="list">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
